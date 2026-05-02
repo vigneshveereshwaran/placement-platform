fa45e0cc87dd19ceb04425c0f0d252d71217bb65
vksarmila0-ops <vksarmila0@gmail.com>
2026-05-02 15:39:48 +0530

    Added notifications and statistics pages

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Server Running"
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
+
+@app.route('/notifications')
+def notifications():
+    return render_template('notifications.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
