84d5785f1cdb0e8fd4decb229e99d0d9a2b0bf58
gayathri <gayathri00671@gmail.com>
2026-05-24 19:10:55 +0530

    updated admin dashboard

---
+++ placement-portal/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("admin/dashboard.html")
+
+@app.route("/add-student")
+def add_student():
+    return render_template("admin/add_student.html")
+
+@app.route("/manage-students")
+def manage_students():
+    return render_template("admin/manage_students.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
