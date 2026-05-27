bdaa51baa6a4969e8379e5ec79eb5a1eeb65ab56
vksarmila0-ops <vksarmila0@gmail.com>
2026-05-27 10:42:32 +0530

    Changed code

--- placement-portal/app.py
+++ placement-portal/app.py
@@ -1,15 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route('/add-student')
 def add_student():
-    return render_template('add_student.html')
+    return render_template('admin/add_student.html')
+
 
 @app.route('/add-company')
 def add_company():
-    return render_template('add_company.html')
+    return render_template('admin/add_company.html')
+
 
 @app.route('/manage-companies')
 def manage_companies():
-    return render_template('manage_companies.html')
+    return render_template('admin/manage_companies.html')
+
 
 @app.route('/notifications')
 def notifications():
-    return render_template('notifications.html')
+    return render_template('notifications/notifications.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
